b1d44b05acb0dd8a363ce7d1f6d4c15035fa6426
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-01-18 13:33:49 -0500

    Add webpack common configuration

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                dependency: { not: ["url"] },
+                type: "javascript/auto",
+                use: ["file-loader"],
+            },
+            {
+                test: /\.(mp3)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[path][name].[ext]", // This keeps the original file name and extension
+                    },
+                },
+            },
+        ],
+    },
+};
