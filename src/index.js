a5ee7e8c907991c5d7770e9926affd024f5b9096
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-01-19 22:33:27 -0500

    Add logic to fetch weather details

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 function weatherApp() {
-    return {};
+    async function getWeatherData(local) {
+        const publicKey = "CWZPRKA3QEL8A7U7HNW4HGMC6";
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${local}?key=${publicKey}`;
+        try {
+            const resultData = await fetch(url, {
+                mode: "cors",
+            });
+
+            if (!resultData.ok) {
+                throw new error(
+                    `Error Status response code ${resultData.status}`
+                );
+            }
+
+            const data = await resultData.json();
+
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return {
+        getWeatherData,
+    };
 }
 
 function weatherAppController() {
@@ -9,3 +33,7 @@ function weatherAppController() {
 function weatherScreenController() {
     return {};
 }
+
+const weather = weatherApp();
+
+weather.getWeatherData("bronx");
